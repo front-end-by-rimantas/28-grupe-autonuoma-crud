56f2935bc8a09b9340a9a3c81b003d5b31a070df
Rimantas <belauzas@yahoo.com>
2021-08-11 18:43:55 +0300

    Solution

--- CarRental.js
+++ CarRental.js
@@ -1,5 +1,60 @@
 class CarRental {
-    constructor() {
+    constructor(companyName) {
+        this.companyName = companyName;
+        this.list = [];
+    }
+
+    intro() {
+        let count = 0;
+        for (const car of this.list) {
+            if (!car.isBroken) {
+                count++;
+            }
+        }
+        console.log(`Hi, we are ${this.companyName} and we have ${count} cars available.`);
+    }
+
+    addCar(model, color, price) {
+        this.list.push({
+            model,
+            color,
+            price,
+            isBroken: false
+        })
+    }
+
+    carPark() {
+        let number = 0;
+        for (const car of this.list) {
+            const { model, color, price, isBroken } = car;
+            if (!isBroken) {
+                console.log(`${++number}. ${model} (${color}) is for ${price}EUR/day.`);
+            }
+        }
+    }
+
+    updateCarPrice(carIndex, newPrice) {
+        this.list[carIndex].price = newPrice;
+    }
+
+    carAccident(carIndex) {
+        this.list[carIndex].isBroken = true;
+    }
+
+    carRepair(carIndex) {
+        this.list[carIndex].isBroken = false;
+    }
+
+    removeCar(carIndex) {
+        const newList = [];
+
+        for (let i = 0; i < this.list.length; i++) {
+            if (i !== carIndex) {
+                newList.push(this.list[i]);
+            }
+        }
+
+        this.list = newList;
     }
 }
 
